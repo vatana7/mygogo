f5eb339e64f8801e99adb81afe9fc41404179092
vatana7 <vatanamain@gmail.com>
2022-04-10 22:46:26 +0700

    Final push

--- src/Location.js
+++ src/Location.js
@@ -1,7 +1,18 @@
 const Location = () => {
     return ( 
         <div className="location">
-            <h1>Bro what the fuck</h1>
+            <div className="inner-location">
+                <div className="header-location">
+                    <div className="location-logo">
+                        <i class="fa fa-compass fa-4x"></i>
+                        <h2><b>LOCATION</b></h2>
+                    </div>
+
+                    <button className="add-button">
+                        ADD LOCATION
+                    </button>
+                </div>
+            </div>
         </div>
      );
 }
